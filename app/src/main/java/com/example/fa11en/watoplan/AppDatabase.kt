4786f4f2706429b23a12ff03f813d5750eda79eb
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2017-11-09 15:32:52 -0500

    working on db connection

--- app/src/main/java/com/example/fa11en/watoplan/AppDatabase.kt
+++ app/src/main/java/com/example/fa11en/watoplan/AppDatabase.kt
@@ -5,8 +5,8 @@ import android.arch.persistence.room.RoomDatabase
 
 
 @Database(entities = {UserEvent.class}, version = 1)
-abstract class AppDatabase : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase () {
 
-    abstract userEventDao () : UserEventDao = 
+    public abstract userEventDao() :
 
 }
--- app/src/main/java/com/example/fa11en/watoplan/UserEventDao.kt
+++ app/src/main/java/com/example/fa11en/watoplan/UserEventDao.kt
@@ -7,7 +7,12 @@ import android.arch.persistence.room.Query
 @Dao
 interface UserEventDao {
 
+    /* eid,  */
+
     @Query('SELECT * FROM event')
-    val getAll(): UserEvent
+    val getAll(): List<UserEvent>
+
+    @Query('SELECT * FROM event WHERE eid IN (:eventIds)')
+    val loadAllByIds(userIds: Array<Int>) : List<UserEvent>
 
 }
