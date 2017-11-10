bc1c828cc322a626588ccbf625a44029a895ade7
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2017-11-10 14:59:52 -0500

    working on type converters, but room is not havin it!

--- app/src/main/java/com/example/fa11en/watoplan/Converters.kt
+++ app/src/main/java/com/example/fa11en/watoplan/Converters.kt
@@ -4,23 +4,23 @@ import android.arch.persistence.room.TypeConverter
 import java.util.*
 
 
-class Converters {
-    companion object {
+object Converters {
 
-        @TypeConverter
-        fun fromTimestamp (stamp: Long?) : Calendar? {
-            if (stamp != null) {
-                val cal = Calendar.getInstance()
-                cal.time = Date(stamp)
-                return cal
-            } else return null
-        }
-
-        @TypeConverter
-        fun calToTimestamp (cal: Calendar?) : Long? {
-            if (cal != null) return cal.timeInMillis
-            else return null
-        }
-
+    @TypeConverter
+    fun fromTimestamp (stamp: Long?) : Calendar? {
+        if (stamp != null) {
+            val cal = Calendar.getInstance()
+            cal.time = Date(stamp)
+            return cal
+        } else return null
     }
+
+    @TypeConverter
+    fun calToTimestamp (cal: Calendar?) : Long? {
+        if (cal != null) return cal.timeInMillis
+        else return null
+    }
+
+
+
 }
