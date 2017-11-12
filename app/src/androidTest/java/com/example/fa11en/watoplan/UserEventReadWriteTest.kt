e0055291b54688f15a4547cedfa9df6c8c1dba75
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2017-11-11 21:07:58 -0500

    finished first test

--- app/src/androidTest/java/com/example/fa11en/watoplan/UserEventReadWriteTest.kt
+++ app/src/androidTest/java/com/example/fa11en/watoplan/UserEventReadWriteTest.kt
@@ -1,12 +1,14 @@
 package com.example.fa11en.watoplan
 
 import android.arch.persistence.room.Room
+import android.location.Location
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 
 
 @RunWith(AndroidJUnit4::class)
@@ -28,9 +30,16 @@ class UserEventReadWriteTest {
     }
 
     @Test
+    @Throws(Exception::class)
     fun writeEventAndReadInList () {
-        val event = UserEvent(0, eventTypes["EVENT"])
-        event.setParams
+        val event = UserEvent(0, eventTypes["EVENT"]!!)
+        event.setParam(ParameterTypes.TITLE, "TEST TITLE")
+        event.setParam(ParameterTypes.DESCRIPTION, "TEST DESCRIPTION")
+        event.setParam(ParameterTypes.DATETIME, Calendar.getInstance())
+        event.setParam(ParameterTypes.LOCATION, Location("gps"))
+        userEventDao.insert(event)
+        val gets: List<UserEvent> = userEventDao.getAll()
+        assert(gets[0] == event)
     }
 
 }
