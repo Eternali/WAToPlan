53ffa7278457bc3af506f1f7eb65cc869869c69b
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2017-11-16 19:26:21 -0500

    working on mvi

--- app/src/main/java/com/example/fa11en/watoplan/views/MainActivity.kt
+++ app/src/main/java/com/example/fa11en/watoplan/views/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.fa11en.watoplan
 
-import android.app.FragmentTransaction
-import android.arch.lifecycle.ViewModelProvider
-import android.arch.lifecycle.ViewModelProviders
-import android.arch.persistence.room.Room
-import android.content.Intent
-import android.location.Location
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.view.*
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.RadioGroup
 import android.widget.ToggleButton
-import com.example.fa11en.watoplan.viewmodel.UserEventViewModel
+import com.example.fa11en.watoplan.views.SummaryView
 import com.getbase.floatingactionbutton.FloatingActionButton
 import com.getbase.floatingactionbutton.FloatingActionsMenu
-import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotterknife.bindView
+import org.jetbrains.annotations.Nullable
+
 
 /*
     Models:
@@ -23,203 +21,61 @@ import java.util.*
         -
  */
 
+class MainActivity (mainState: SummaryViewState): AppCompatActivity (), SummaryView {
 
-fun MutableList<UserEvent>.loadAll (db: AppDatabase) : Boolean {
-    this.clear()
-    db.beginTransaction()
-    return try {
-        this.addAll(db.eventDao().getAll())
-        db.setTransactionSuccessful()
-        true
-    } catch (e: Exception) {
-        false
-    } finally {
-        db.endTransaction()
-    }
-}
-
-fun MutableList<UserEvent>.saveAll (db: AppDatabase) : Boolean {
-    db.beginTransaction()
-    return try {
-        this.forEach {
-            db.eventDao().insert(it)
-        }
-        db.setTransactionSuccessful()
-        true
-    } catch (e: Exception) {
-        false
-    } finally {
-        db.endTransaction()
-    }
-}
-
-fun MutableList<UserEvent>.deleteEvent (pos: Int, db: AppDatabase) : Boolean {
-    db.beginTransaction()
-    return try {
-        db.eventDao().delete(this[pos])
-        db.setTransactionSuccessful()
-        true
-    } catch (e: Exception) {
-        false
-    } finally {
-        this.removeAt(pos)
-        db.endTransaction()
-    }
-}
-
-fun MutableList<UserEvent>.addEvent (event: UserEvent, db: AppDatabase) : Boolean {
-    db.beginTransaction()
-    return try {
-        db.eventDao().insert(event)
-        db.setTransactionSuccessful()
-        true
-    } catch (e: Exception) {
-        false
-    } finally {
-        this.add(event)
-        db.endTransaction()
-    }
-}
+    // use kotterknife to bind views to vars
+    private val addMenu: FloatingActionsMenu by bindView(R.id.addMenu)
 
 
-enum class ParameterTypes (val param: String){
-    TITLE ("TITLE"),
-    DESCRIPTION ("DESCRIPTION"),
-    DATETIME ("DATETIME"),
-    LOCATION ("LOCATION"),
-    ENTITIES ("ENTITIES"),
-    REPEAT ("REPEAT")
-}
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
 
-internal val eventTypes = hashMapOf(
-        "EVENT" to EventType("EVENT",
-                                    arrayListOf(ParameterTypes.TITLE,
-                                            ParameterTypes.DESCRIPTION,
-                                            ParameterTypes.DATETIME,
-                                            ParameterTypes.LOCATION,
-                                            ParameterTypes.ENTITIES)),
-        "EVALUATION" to EventType("EVALUATION",
-                                    arrayListOf(ParameterTypes.TITLE,
-                                            ParameterTypes.DESCRIPTION,
-                                            ParameterTypes.DATETIME)),
-        "PROJECT" to EventType("PROJECT",
-                                    arrayListOf(ParameterTypes.TITLE,
-                                            ParameterTypes.DESCRIPTION,
-                                            ParameterTypes.DATETIME)),
-        "REMINDER" to EventType("REMINDER",
-                                    arrayListOf(ParameterTypes.TITLE,
-                                            ParameterTypes.DESCRIPTION,
-                                            ParameterTypes.DATETIME))
-        )
-
-internal var appdb: AppDatabase? = null
-//internal var events: MutableList<UserEvent> = ArrayList()
-
-class MainActivity : AppCompatActivity() {
-
-    val displayToggleListener: RadioGroup.OnCheckedChangeListener = RadioGroup.OnCheckedChangeListener { group, checkedId ->
-        for (r in 0..group.childCount) {
-            if (group.getChildAt(r) == null) continue
-            val view: ToggleButton = group.getChildAt(r) as ToggleButton
-            view.isChecked = view.id == checkedId
-        }
-    }
-
-    lateinit private var eventsModel: UserEventViewModel
-    lateinit private var dotMenu: Menu
-    lateinit var displayGroup: RadioGroup
-    lateinit var dayToggle: ToggleButton
-    lateinit var weekToggle: ToggleButton
-    lateinit var monthToggle: ToggleButton
-
-    lateinit var addMenu: FloatingActionsMenu
-    val addMenuButtons: MutableList<FloatingActionButton> = mutableListOf()
-
-    fun toggleDisplay (view: View) {
-        displayGroup.clearCheck()
-        displayGroup.check(view.id)
-
-        val fragTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        when (view.id) {
-            R.id.dayToggle -> {
-                fragTransaction.replace(R.id.displayFragContainer, DayFragment(), "Day")
-            }
-            R.id.weekToggle -> {
-                fragTransaction.replace(R.id.displayFragContainer, WeekFragment(), "Week")
-            }
-            R.id.monthToggle -> {
-                fragTransaction.replace(R.id.displayFragContainer, MonthFragment(), "Month")
-            }
-        }
-        fragTransaction.commit()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        // set content view to layout
+        super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
 
-        appdb = EventsDB.getInstance(this)
-        eventsModel = ViewModelProviders.of(this).get(UserEventViewModel::class.java)
+        render(mainState,this)
 
-        displayGroup = findViewById(R.id.overviewLayoutSwitcher)
-        dayToggle = findViewById(R.id.dayToggle)
-        weekToggle = findViewById(R.id.weekToggle)
-        monthToggle = findViewById(R.id.monthToggle)
+    }
 
-        displayGroup.setOnCheckedChangeListener(displayToggleListener)
+    override fun editIntent(ctx: Context, event: UserEvent): UserEvent {
 
-        val fragTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragTransaction.replace(R.id.displayFragContainer, WeekFragment(), "Week")
-        fragTransaction.commit()
+    }
 
-        addMenu = findViewById<FloatingActionsMenu>(R.id.addMenu)
+    override fun addIntent(ctx: Context, type: EventType): EventType {
 
-        // generate event adding buttons
-        eventTypes.forEach {
-            val button = FloatingActionButton(this)
+    }
+
+    override fun render (summaryState: SummaryViewState, ctx: Context) {
+        // get togglers from layout
+        val displayGroup = findViewById<RadioGroup>(R.id.displayToggleGroup)
+        val dayToggle = findViewById<ToggleButton>(R.id.dayToggle)
+        val weekToggle = findViewById<ToggleButton>(R.id.weekToggle)
+        val monthToggle = findViewById<ToggleButton>(R.id.monthToggle)
+
+        // make radio group from togglers
+        val displayToggleListener: RadioGroup.OnCheckedChangeListener = RadioGroup.OnCheckedChangeListener
+        displayGroup.setOnCheckedChangeListener({group, checkedId ->
+            for (t in 0..group.childCount) {
+                if (group.getChildAt(t) == null) continue
+                val view: ToggleButton = group.getChildAt(t) as ToggleButton
+                view.isChecked = view.id == checkedId
+            }
+        })
+
+        // generate FABS
+        mainState.eventTypes.forEach {
+            val button = FloatingActionButton(ctx)
             button.size = FloatingActionButton.SIZE_MINI
-            button.setColorNormalResId(R.color.colorAccent)
-            button.setColorPressedResId(R.color.colorAccent_pressed)
-            button.title = it.key
+            button.setColorNormalResId(it.colorNormal)
+            button.setColorPressedResId(it.colorPressed)
+            button.title = it.name
             button.setOnClickListener { _ ->
                 addMenu.collapse()
-                val editIntent = Intent(this, EditActivity::class.java)
-                editIntent.putExtra("typeName", it.key)
-                startActivity(editIntent)
+                addIntent(ctx, it)
             }
-
-            addMenuButtons.add(button)
-            addMenu.addButton(addMenuButtons[addMenuButtons.size-1])
+            addMenu.addButton(button)
         }
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        if (menu == null) return false
-        menuInflater.inflate(R.menu.dot_menu, menu)
-        dotMenu = menu
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if (item == null) return false
-        when (item.itemId) {
-            R.id.action_settings -> {
-                val settingsIntent = Intent(this, SettingsActivity::class.java)
-                startActivity(settingsIntent)
-            }
-        }
-
-        return true
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_MENU) {
-            dotMenu.performIdentifierAction(R.id.dot_menu, 0)
-            return true
-        }
-
-        return super.onKeyDown(keyCode, event)
     }
 
 }
+
---
+++ app/src/main/java/com/example/fa11en/watoplan/views/SummaryView.kt
@@ -0,0 +1,23 @@
+package com.example.fa11en.watoplan.views
+
+import android.content.Context
+import com.example.fa11en.watoplan.EventType
+import com.example.fa11en.watoplan.UserEvent
+
+
+interface SummaryView {
+
+    // click on an event intent
+    fun editIntent (): UserEvent
+
+    // click on a FAB
+    fun addIntent (ctx: Context, type: EventType): EventType
+
+    /*
+        Render the view
+        @param summaryState: the viewState to be displayed
+        @param ctx: the context to be rendered in
+    */
+    fun render (summaryState: SummaryViewState, ctx: Context)
+
+}
